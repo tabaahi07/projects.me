e590ae566c7f7af063446f070a47c9fb06e03608
tabaahi07 <126075036+tabaahi07@users.noreply.github.com>
2023-07-25 21:19:25 +0530

    Done! Project component setup

--- src/Mobile/Components/Mobile.jsx
+++ src/Mobile/Components/Mobile.jsx
@@ -2,6 +2,7 @@ import React , {useState}  from "react";
 import styles from '../CSS/Mobile.module.css' ;
 import Header from "./Header";
 import HambergerMenu from "./HambergerMenu";
+import ProjectList from "./ProjectList";
 
 const Mobile = ()=> {
    const [showSocialLinks , setShowSocialLinks] = useState(false) ;
@@ -9,6 +10,7 @@ const Mobile = ()=> {
    return <div className={styles.Mobile} >
        <Header showSocialLinks={showSocialLinks} setShowSocialLinks={setShowSocialLinks} /> 
        {showSocialLinks && <HambergerMenu> </HambergerMenu>}
+       <ProjectList> </ProjectList>
    </div>
 }
 
--- src/config.js
+++ src/config.js
@@ -10,14 +10,16 @@ const config ={
     projects : [
         {
             projectID : 0 ,
-            projectName : "" ,
-            projectScreenshot : "" ,
+            projectName : "Advice Generator App" ,
+            projectScreenshot : "https://th.bing.com/th/id/OIP.Z_PIeIRDajXPmZHROt-T_QHaEK?w=280&h=180&c=7&r=0&o=5&pid=1.7" ,
             projectTechnologies : [
                 
             ] ,
             projectDescription : ""
         }
     ]
+
+     
 
 
 }
