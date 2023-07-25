cf81dcce8c4bb0c740e73e102ec24acb789eb2e8
tabaahi07 <126075036+tabaahi07@users.noreply.github.com>
2023-07-25 15:12:58 +0530

    config file done!

---
+++ src/config.js
@@ -0,0 +1,21 @@
+export default config ={
+    name : "Akrati Pandey" ,
+    socialLinks : {
+        github : "https://github.com/tabaahi07" ,
+        linkedin : "https://www.linkedin.com/in/akrati-pandey-a30b49273/" ,
+        instagram : "https://www.instagram.com/tabaahii_07/"
+    } ,
+
+    projects : [
+        {
+            projectName : "" ,
+            projectScreenshot : "" ,
+            projectTechnologies : [
+                
+            ] ,
+            projectDescription : ""
+        }
+    ]
+
+
+}
