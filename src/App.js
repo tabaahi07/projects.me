d6b3d3e6f3de80fd40e8198c6ab8649437cd879e
Rahul Goel <211220042@nitdelhi.ac.in>
2023-07-25 15:32:50 +0530

    Done Header and ProjectList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Desktop from './Desktop/Components/Desktop';
 
 function App() {
   return (
     <div className="App">
-      
+      <Desktop />
     </div>
   );
 }
---
+++ src/Desktop/Components/ProjectList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "../css/ProjectList.module.css"
+
+const ProjectList = () => {
+    return (
+        <div className={styles.ProjectList}>
+            
+        </div>
+    );
+}
+
+export default ProjectList;
