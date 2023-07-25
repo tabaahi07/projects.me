fb86330bc98bd8abd4ff9ce49a767348ccc7d805
Rahul Goel <211220042@nitdelhi.ac.in>
2023-07-25 22:24:16 +0530

    icons and config change

--- src/config.js
+++ src/config.js
@@ -1,25 +1,36 @@
-const config ={
-    name : "Akrati Pandey" ,
-    resumeLink : "" ,
-    socialLinks : {
-        github : "https://github.com/tabaahi07" ,
-        linkedin : "https://www.linkedin.com/in/akrati-pandey-a30b49273/" ,
-        instagram : "https://www.instagram.com/tabaahii_07/"
-    } ,
+const Technologies = {
+    Reactjs: "reactjs",
+    HTML: "html",
+    CSS3: "css3",
+    JS: "js",
+    API: "api",
+    BOOTSTRAP: "bootstrap",
+    TAILWINDCSS: "tailwind-css"
+}
 
-    projects : [
+const config = {
+    name: "Akrati Pandey",
+    resumeLink: "",
+    socialLinks: {
+        github: "https://github.com/tabaahi07",
+        linkedin: "https://www.linkedin.com/in/akrati-pandey-a30b49273/",
+        instagram: "https://www.instagram.com/tabaahii_07/"
+    },
+
+    projects: [
         {
-            projectID : 0 ,
-            projectName : "" ,
-            projectScreenshot : "" ,
-            projectTechnologies : [
-                
-            ] ,
-            projectDescription : ""
+            projectID: 0,
+            projectName: "Random Advice Generator",
+            projectScreenshotID: "1-lYYzMJahByaHQ6SWpK00Q_PAlc1574m",
+            projectTechnologies: [
+                Technologies.Reactjs, Technologies.API, Technologies.JS, Technologies.HTML, Technologies.CSS3
+            ],
+            projectDescription: "I tell you about project! Project is big, big website made with website coding language, yes, yes! It has many colors, like rainbow, and pictures of funny animals. Maybe it's about animals or maybe about food, I not sure. But I think it has big buttons that make funny noises when you click! Boop boop! And there are many words, like story, story everywhere!"
         }
     ]
 
 
 }
 
-export default config ;
+export default config;
+export {Technologies}
